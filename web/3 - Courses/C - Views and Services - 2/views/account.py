2f0116aecdf302a7c9212875a994e93d84876c1c
jgalamba <jfgalamba@mail>
2023-03-07 10:52:49 +0000

    Added viewmodels to account views

--- web/3 - Courses/C - Views and Services - 2/views/account.py
+++ web/3 - Courses/C - Views and Services - 2/views/account.py
@@ -1,7 +1,34 @@
 from fastapi import APIRouter
 from fastapi_chameleon import template
+from common import base_viewmodel_with
 
 router = APIRouter()
+
+@router.get('/account/register')                            # type: ignore
+@template()
+async def register():
+    return register_viewmodel()
+#:
+
+def register_viewmodel():
+    return base_viewmodel_with({
+        'error': False,
+        'error_msg': 'There was an error with your data. Please try again.',
+    })
+#:
+
+@router.get('/account/login')                            # type: ignore
+@template()
+async def login():
+    return login_viewmodel()
+#:
+
+def login_viewmodel():
+    return base_viewmodel_with({
+        'error': None,
+        # 'error_msg': 'There was an error with your data. Please try again.',
+    })
+#:
 
 @router.get('/account')                            # type: ignore
 async def index():
@@ -9,22 +36,3 @@ async def index():
 
     }
 #:
-
-@router.get('/account/register')                            # type: ignore
-@template()
-async def register():
-    return {
-        'error': False,
-        'error_msg': 'There was an error with your data. Please try again.'
-    }
-#:
-
-@router.get('/account/login')                            # type: ignore
-@template()
-async def login():
-    return {
-        'error': False,
-        'error_msg': 'There was an error with your data. Please try again.'
-    }
-#:
-
